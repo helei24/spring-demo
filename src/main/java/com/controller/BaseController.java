df84c86dca179ab5592e4cd5a3e3117d78bea9a7
He Lei <he.lei@integrasco.no>
2014-12-18 10:52:32 +0800

    add new files.

---
+++ src/main/java/com/controller/BaseController.java
@@ -0,0 +1,12 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BaseController {
+
+    public void hello(HttpServletRequest request){
+        
+        request.get
+    }
+}
