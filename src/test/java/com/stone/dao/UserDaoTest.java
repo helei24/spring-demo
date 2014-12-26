954765f2ccfee748ae3fad9f704f5a6fa415e88c
He Lei <he.lei@integrasco.no>
2014-12-27 00:51:18 +0800

    update

---
+++ src/test/java/com/stone/dao/UserDaoTest.java
@@ -0,0 +1,27 @@
+package com.stone.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.stone.domain.User;
+
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserDaoTest {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void test_findUserByName() {
+
+        User user = userDao.findUserByName("test");
+
+        Assert.assertEquals("test", user.getName());
+    }
+
+}
